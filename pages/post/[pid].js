93571493f5ec5ba2d0a9f59f6258237b44f7b36f
gusflopes <gusflopes86@gmail.com>
2020-04-02 10:30:43 -0400

    feat: static and client side at same page

--- pages/post/[pid].js
+++ pages/post/[pid].js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useRouter } from 'next/router'
 
 // import { Container } from './styles';
 
 const Post = () => {
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState('')
   const router = useRouter();
   const { pid } = router.query;
+
+  useEffect(() => {
+    setData([pid])
+  }, [pid])
+
+  async function handleClick() {
+    setData(data => [...data, input]);
+    setInput('');
+  }
 
   return (
     <div>
       <h1>Post: {pid} </h1>
+
+      <input value={input} onChange={e => setInput(e.target.value)} />
+      <button onClick={handleClick}>Adicionar</button>
       <p>Get the Pid params from routes.</p>
+      <ul>
+        {data.map(d => (
+          <li key={`element-${d}`}>{d} <button onClick={() => alert(`Element Key: ${d}`)} > (-) </button></li>
+        ))}
+      </ul>
+      <button onClick={() => console.log(data)}>Log</button>
     </div>
   );
 }
